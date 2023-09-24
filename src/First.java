d8765be1dea063b7109eea1acea2150938da5399
Rohit4702 <rsahu0308@gmail.com>
2023-09-24 23:23:31 +0530

    First Commit

---
+++ src/First.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+        char grade ='B';
+        //type casting
+        grade= (char) (grade+8);
+        System.out.println(grade);
+        //decrypting
+        System.out.println((char) (grade-8));
+    }
+}
